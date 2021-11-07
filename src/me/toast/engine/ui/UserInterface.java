1781c6349aa85c73f83b1bb6e24bbfeeb2201870
Litl Toast <joshuafhiggins@gmail.com>
2021-11-07 15:42:49 -0500

    Downgraded Ultralight to v1.2.1
    No errors and running, but no idea if functional yet.

--- src/me/toast/engine/ui/UserInterface.java
+++ src/me/toast/engine/ui/UserInterface.java
@@ -16,25 +16,24 @@ public class UserInterface {
 
         //Windows PATH in Java can't be changed at runtime. Needs to be done ahead of time
         //TODO: Test this in linux if the new binaries work
-        if (OperatingSystem.get() != OperatingSystem.WINDOWS) {
-            // Get the existing native library path
-            String libraryPath = System.getProperty("java.library.path");
-            if (libraryPath != null) {
-                // There is a path set already, append our natives dir
-                libraryPath += File.pathSeparator + nativesDir.toAbsolutePath();
-            } else {
-                // There is no path set, make our natives dir the current path
-                libraryPath = nativesDir.toAbsolutePath().toString();
-            }
-            System.setProperty("java.library.path", libraryPath);
+
+        // Get the existing native library path
+        String libraryPath = System.getProperty("java.library.path");
+        if (libraryPath != null) {
+            // There is a path set already, append our natives dir
+            libraryPath += File.pathSeparator + nativesDir.toAbsolutePath();
+        } else {
+            // There is no path set, make our natives dir the current path
+            libraryPath = nativesDir.toAbsolutePath().toString();
         }
+        System.setProperty("java.library.path", libraryPath);
 
         try {
-            UltralightGPUDriverNativeUtil.extractNativeLibrary(nativesDir);
             UltralightJava.extractNativeLibrary(nativesDir);
+            UltralightGPUDriverNativeUtil.extractNativeLibrary(nativesDir);
 
-            UltralightGPUDriverNativeUtil.load(nativesDir);
             UltralightJava.load(nativesDir);
+            UltralightGPUDriverNativeUtil.load(nativesDir);
         } catch (UltralightLoadException e) {
             e.printStackTrace();
         }
