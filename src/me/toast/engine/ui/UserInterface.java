0af29b84095446495ff86d6ede69c41427dc724c
Litl Toast <joshuafhiggins@gmail.com>
2021-12-11 18:31:49 -0500

    Sizing works
    shouldn't have taken a day

--- src/me/toast/engine/ui/UserInterface.java
+++ src/me/toast/engine/ui/UserInterface.java
@@ -1,21 +1,11 @@
 package me.toast.engine.ui;
 
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.SnapshotParameters;
-import javafx.scene.SnapshotResult;
-import javafx.scene.control.Button;
-import javafx.scene.image.PixelFormat;
-import javafx.scene.image.WritableImage;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.scene.image.*;
 import javafx.scene.layout.VBox;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
-import javafx.util.Callback;
-import me.toast.engine.Mod;
 
 import java.nio.ByteBuffer;
 import java.util.concurrent.Semaphore;
@@ -59,32 +49,21 @@ public class UserInterface extends Application {
         stage.setTitle("Webview");
         stage.setResizable(false);
 
-        stage.setMinHeight(720);
-        stage.setMinWidth(1280);
-        stage.setMaxHeight(720);
-        stage.setMaxWidth(1280);
         stage.setHeight(720);
         stage.setWidth(1280);
 
         webView = new WebView();
-        webView.getEngine().load("https://higgy999.github.io/");
-
         webView.setPrefSize(1280, 720);
         webView.setMaxSize(1280, 720);
         webView.setMinSize(1280, 720);
 
+        webView.getEngine().load("https://higgy999.github.io/");
+
         VBox vBox = new VBox(webView);
-        vBox.setPrefSize(1280, 720);
-        vBox.setMaxSize(1280, 720);
-        vBox.setMinSize(1280, 720);
 
         FrameToCapture = new Scene(vBox, 1280, 720);
         stage.setScene(FrameToCapture);
-
         stage.sizeToScene();
-
         stage.show();
-
-        stage.sizeToScene();
     }
 }
